ce3ad2e3ecf3e2367a619850febe1ed6d6cdf4e3
MyComputer <MyComputer@MyComputer>
2023-01-22 20:00:34 +0300

    fix language

--- AQA/src/hw1/Application.java
+++ AQA/src/hw1/Application.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class Application {
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите, пожалуйста, первое числовое значение!");
+		System.out.println("Please enter the first numeric value!");
 		String firstStr = scan.nextLine();
 		
 		RegEx value1 = new RegEx(firstStr);
 		if(value1.checkValue() == false) {
-			System.out.println("Вы ввели нечисловое значение или знак \",\"!");
+			System.out.println("You entered a non-numeric value or character \",\"!");
 			return;
 		}
 		
-		System.out.println("Введите, пожалуйста, второе числовое значение!");
+		System.out.println("Please enter the second numeric value!");
 		String secondStr = scan.nextLine();
 		
 		RegEx value2 = new RegEx(secondStr);
 		if(value2.checkValue() == false) {
-			System.out.println("Вы ввели нечисловое значение или знак \",\"!");
+			System.out.println("You entered a non-numeric value or character \",\"!");
 			return;
 		}
 		
@@ -28,10 +29,10 @@ public class Application {
 		Values multVal = (x, y) -> x * y;
 		Values divVal = (x, y) -> x / y;
 		
-		System.out.println("Сложение: " + sumVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Вычитание: " + subVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Умножение: " + multVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Деление: " + divVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("Addition: " + sumVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("Subtraction: " + subVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("Multiplication: " + multVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("Division: " + divVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
 		
 		scan.close();
 	}
