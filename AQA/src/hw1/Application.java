4e36a1804704311bdbe9614ff2ecb34191287598
MyComputer <MyComputer@MyComputer>
2023-01-22 19:19:01 +0300

    fix 2

--- AQA/src/hw1/Application.java
+++ AQA/src/hw1/Application.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 public class Application {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите, пожалуйста, первое числовое значение!");
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!");
 		String firstStr = scan.nextLine();
 		
 		RegEx value1 = new RegEx(firstStr);
-		value1.checkValue();
 		if(value1.checkValue() == false) {
-			System.out.println("Вы ввели нечисловое значение либо знак \",\"!");
+			System.out.println("пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ \",\"!");
 			return;
 		}
 		
-		System.out.println("Введите, пожалуйста, второе числовое значение!");
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!");
 		String secondStr = scan.nextLine();
 		
 		RegEx value2 = new RegEx(secondStr);
-		value2.checkValue();
 		if(value2.checkValue() == false) {
-			System.out.println("Вы ввели нечисловое значение либо знак \",\"!");
+			System.out.println("пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ \",\"!");
 			return;
 		}
 		
@@ -30,10 +28,10 @@ public class Application {
 		Values multVal = (x, y) -> x * y;
 		Values divVal = (x, y) -> x / y;
 		
-		System.out.println("Сложение: " + sumVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Вычитание: " + subVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Умножение: " + multVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
-		System.out.println("Деление: " + divVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("РЎР»РѕР¶РµРЅРёРµ: " + sumVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: " + subVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: " + multVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: " + divVal.getValue(Double.valueOf(firstStr), Double.valueOf(secondStr)));
 		
 		scan.close();
 	}
